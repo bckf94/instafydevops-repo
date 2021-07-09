3b6faaacd43d98739812d3e01b913806a9f68a5a
Florian.Beck <Florian.Beck@doubleslash.de>
2021-07-09 11:01:13 +0200

    add local authentication

--- app/Program.cs
+++ app/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.Azure.Functions.Authentication.WebAssembly;
+using AzureStaticWebApps.Blazor.Authentication;
 
 namespace InstafyDemo
 {
@@ -18,9 +18,10 @@ namespace InstafyDemo
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddStaticWebAppsAuthentication();
-
+         builder.Services
+                .AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
+                .AddStaticWebAppsAuthentication();
+          
             await builder.Build().RunAsync();
         }
     }
